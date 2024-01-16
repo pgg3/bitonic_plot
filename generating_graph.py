78161cd1f4c406bb16ebff207e69abd8c0e9a069
pgg3 <carljackson888777@gmail.com>
2024-01-16 20:16:36 +0800

    Update

--- generating_graph.py
+++ generating_graph.py
@@ -1,53 +1,158 @@
-import networkx as nx
+
 import matplotlib.pyplot as plt
 import math
+plt.rcParams.update({
+    "font.family": "serif",  # use serif/main font for text elements
+    "font.serif": "Times New Roman",
+    "font.sans-serif": "Times New Roman",
+    "font.monospace": "Inconsolata",
+    "mathtext.fontset": "custom",
+    "mathtext.cal": "txsys",
+    "ps.usedistiller": 'xpdf',
+    "ps.fonttype" : 42,
+    "pdf.fonttype" : 42,
+    "axes.labelpad": 0,
+    "font.weight": "bold",
+    "savefig.bbox": "tight",
+    "savefig.pad_inches": 0.01,
+    "axes.unicode_minus": False
+})
 
-def add_comparator(G, u, v, stage, index):
-    G.add_edge((u, stage), (v, stage), index=index)
-
-def create_bitonic_sorting_network(n):
-    G = nx.DiGraph()
-    logn = int(math.log2(n))
-
-    for i in range(logn):
-        for j in range(n):
-            index = (j // (2 ** i)) % 2
-            partner = j ^ (2 ** i)
-            if partner > j:
-                if index == 0:
-                    # Ascending
-                    add_comparator(G, j, partner, i, j // (2 ** (i + 1)))
-                else:
-                    # Descending
-                    add_comparator(G, partner, j, i, j // (2 ** (i + 1)))
-
-    return G
+plot_dict = []
+def bitonic_sort_draw(N):
+    n_list = list(range(N))
+    for k in range(1,int(math.log(N, 2))+1):
+        for j in range(k-1, -1, -1):
+            for i in range(N):
+                temp = i ^ (2 ** j)
+                if temp > i:
+                    if i & (2 ** k) == 0:
+                        plot_dict.append((i, temp, 0))
+                    else:
+                        plot_dict.append((i, temp, 1))
 
 if __name__ == '__main__':
 
-    # Initialize a directed graph
-    G = nx.DiGraph()
+    new_fig = plt.figure(
+        figsize=(16, 16)
+    )
+    new_ax = new_fig.add_subplot(111)
+    # set all spines to invisible
+    new_ax.spines['top'].set_color('none')
+    new_ax.spines['bottom'].set_color('none')
+    new_ax.spines['left'].set_color('none')
+    new_ax.spines['right'].set_color('none')
+    # set all ticks to invisible
+    new_ax.tick_params(labelcolor='w', top=False, bottom=False, left=False, right=False)
 
-    # Assuming we have 8 elements to sort, we create 8 levels
-    levels = 8
+    # NUM_TO_SORT
+    N = 16
 
-    # Create nodes, each level will have nodes equal to the number of elements to sort
-    for level in range(levels):
-        for i in range(levels):
-            G.add_node((level, i), pos=(level, i))
+    # create input-output lines
+    for i in range(N):
+        new_ax.hlines(
+            y=i,
+            xmin=0,
+            xmax=N**2,
+            color='k',
+            linewidth=1,
+            zorder=-1
+        )
+        new_ax.text(
+            x=N**2+0.2,
+            y=i,
+            s=str(i),
+            color='k',
+            verticalalignment='center'
+        )
+        new_ax.text(
+            x=-0.5,
+            y=i,
+            s=str(i),
+            color='k',
+            verticalalignment='center'
+        )
 
-    # Connect nodes to simulate comparisons and swaps
-    # This is where you would implement the logic of your sorting network
-    # For demonstration, this will just connect each node to the next level node
-    for level in range(levels - 1):
-        for i in range(levels):
-            G.add_edge((level, i), (level + 1, i))
+    # draw bitonic sort comparisons
+    bitonic_sort_draw(N)
 
-    # Extract positions
-    pos = nx.get_node_attributes(G, 'pos')
+    i = 0
+    line_width = 1
+    arrow_width = 0.3
+    head_width = 2*line_width
+    color = 'darkorange'
+    stage_distance = 50
+    inner_stage_distance = 20
+    move_distance = 2
+    for k in range(1, int(math.log(N, 2))+1):
+        for j in range(k):
+            movement = 0
+            for z in range(int(N/2)):
+                compare = plot_dict[i]
+                regret = 2**(k - j - 1)
+                if compare[2] == 0:
+                    new_ax.arrow(
+                        x=movement*move_distance+stage_distance*k+inner_stage_distance*j,
+                        y=compare[0],
+                        dx=0,
+                        dy=compare[1] - compare[0] - arrow_width,
+                        color=color,
+                        linewidth=line_width,
+                        shape='full',
+                        width=arrow_width,
+                        head_length=arrow_width,
+                        head_width=head_width
+                    )
+                else:
+                    new_ax.arrow(
+                        x=movement*move_distance+stage_distance*k+inner_stage_distance*j,
+                        y=compare[1],
+                        dx=0,
+                        dy=compare[0] - compare[1] + arrow_width,
+                        color=color,
+                        linewidth=line_width,
+                        shape='full',
+                        width=arrow_width,
+                        head_length=arrow_width,
+                        head_width=head_width
+                    )
+                movement += 1
+                movement %= regret
+                i += 1
 
-    # Draw the graph using the positions we set for each node
-    nx.draw(G, pos, with_labels=False, node_size=500, node_color="orange", arrows=True)
+    # i = 0
+    #
+    # for compare in plot_dict:
+    #     if compare[2] == 0:
+    #         new_ax.arrow(
+    #             x=i,
+    #             y=compare[0],
+    #             dx=0,
+    #             dy=compare[1]-compare[0]-arrow_width,
+    #             color=color,
+    #             linewidth=line_width,
+    #             shape='full',
+    #             width=arrow_width,
+    #             head_length=arrow_width
+    #         )
+    #     else:
+    #         new_ax.arrow(
+    #             x=i,
+    #             y=compare[1],
+    #             dx=0,
+    #             dy=compare[0]-compare[1]+arrow_width,
+    #             color=color,
+    #             linewidth=line_width,
+    #             shape='full',
+    #             width=arrow_width,
+    #             head_length=arrow_width
+    #         )
+    #     i += 1
 
+
+    # invert y-axis, x-axis
+    new_ax.invert_yaxis()
+    # new_ax.invert_xaxis()
     # Show the plot
+    plt.savefig('test.pdf')
     plt.show()
